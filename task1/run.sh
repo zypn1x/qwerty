c8b8a709e631be8489870d4a4498416d4dd966cf
zypnix <sadik212003@gmail.com>
2022-03-05 23:42:44 +0300

    code style in run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
 
+
 while [ -n "$1" ]
-do
-case $1 in
-"--input_folder")
-input_folder=$2
-;;
-"--extension")
-extension=$2
-;;
-"--backup_folder")
-backup=$2
-;;
-"--backup_archive_name")
-name=$2
-;;
-esac
-shift 2
+	do
+	case $1 in
+	"--input_folder")
+		input_folder=$2
+		;;
+	"--extension")
+		extension=$2
+		;;
+	"--backup_folder")
+		backup=$2
+		;;
+	"--backup_archive_name")
+		name=$2
+		;;
+	esac
+	shift 2
 done
 
 mkdir $backup
